48a031a170cec67da2f7d782c61a0a247a212588
Bagas Noor <19623186@std.stei.itb.ac.id>
2025-03-02 13:49:35 +0700

    feat: added getPlaylist by using endpoint API provided by Spotify

--- spotify.js
+++ spotify.js
@@ -8,18 +8,37 @@ async function getAccessToken(params) {
         headers: {
             'Content-Type' : 'application/x-www-form-urlencoded',
             'Authorization' : 'Basic ' + btoa( clientId + ':' + clientSecret)
-        },
+        }, // Spotify Requirements btoa for authentication request
         body: 'grant_type=client_credentials'
     });
 
     const data = await result.json();
+    console.log(data)
     return data.access_token;
+    
+}
 
+async function getPlaylist(){
+    
+    const Token = await getAccessToken();
+    console.log(Token);
+
+    const result = await fetch ('https://api.spotify.com/v1/playlists/7dyetqSF5Bgd1lEkr9UzI1', {
+        method : 'GET',
+        headers : {
+            Authorization : `Bearer ${Token}`
+        }
+    })
+
+    const data = await result.json();
+    console.log(data);
 }
 
 async function test(params) {
     const token = await getAccessToken();
     console.log("Access Token:", token);
+    getPlaylist();
 }
+
 
 test();
